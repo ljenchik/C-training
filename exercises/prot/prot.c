bb4654f2fb88a75ec5a2e10af3b25d67c71d1a92
ljenchik <pelagenkolena@gmail.com>
2024-01-06 22:44:05 +0000

    Prot exercise

--- exercises/prot/prot.c
+++ exercises/prot/prot.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 int main(void) {
-  int Prot = 0xB7B2;
+  unsigned short int Prot = 0xB7B2;
   
-  // id = prot >> (sizeof(int) - 7)
+  int id1 = Prot >> 13;
   printf("%d\n", Prot);
+  printf("%d\n", id1);
 
+  int id2 = ((unsigned short)(Prot << 3)) >> 10;
+  printf("%d\n", id2);
+  
+  int id3 = ((unsigned short)(Prot <<9 )) >> 9;
+  printf("%d\n", id3);
+
+  int check = (id1 << 13) + (id2 << 7) + id3;
+  printf("%d\n", check);
   return 0;
-}
+}  
