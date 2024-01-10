8aae097c3bd24062e18f828acdb95e27dfcba5e7
ljenchik <pelagenkolena@gmail.com>
2024-01-10 13:30:16 +0000

    Added changes to prot_3

--- exercises/data_structures/prot/prot_3.c
+++ exercises/data_structures/prot/prot_3.c
@@ -6,15 +6,24 @@
 int main(void) {
   unsigned short int prot = 0xB7B2;
 
+// 1011011110110010 = 0xB7B2 = 47026
+// prot >> 10 = 0000000000101101 = 45
   int type = prot >> 10;
   printf("Type: %d\n", type);
 
-
+// prot >> 7 = 0000000101101111 
+//       0x7 = 0000000000000111
+// prot >> 7 & 0x7 = 0000000000000111 = 7
  int priority = prot >> 7 & 0x7;
   printf("Priority: %d\n", priority);
 
-  
- int id = (prot << 9) >> 9 & 0x7f;
+
+  // prot << 9 = 01100100000000
+  // (prot << 9) >> 9 = 0000000000110010
+  //             0x7f = 0000000001111111
+  // (prot << 9) >> 9 & 0x7f = 0000000000110010 = 50
+ //int id = (prot << 9) >> 9 & 0x7f;
+  int id = prot & 0x7f;
   printf("Priority: %d\n", id);
 
 
