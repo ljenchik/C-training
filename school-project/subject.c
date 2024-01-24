a490463c3c32149348183451438db1584b422359
ljenchik <pelagenkolena@gmail.com>
2024-01-24 10:34:10 +0000

    Added two bonus functions

--- school-project/subject.c
+++ school-project/subject.c
@@ -339,6 +339,41 @@ void print_grade_per_subject(Student *student, int subject_id) {
       printf("%d \n", found_subject->subject.grade);
     }
   }
+
+// Find which teachers teach a specified student
+void print_teachers_by_student(StudentList *head, int student_id) {
+  StudentList *found_student = get_student_by_id(head, student_id);
+  SubjectList *existing_subjects = found_student->student.student_subjects;
+  if (existing_subjects != NULL) {
+    printf("%s\'s teachers \n", found_student->student.student_name);
+    SubjectList *current_subjects = existing_subjects;
+    while (current_subjects != NULL) {
+      printf("%s \n", current_subjects->subject.teacher);
+      current_subjects = current_subjects->next;
+    }
+  }
+  else {
+    printf("No teachers were assigned to %s \n", found_student->student.student_name);
+  }
+}
+
+
+
+// Find which students are taught by a specified teacher
+void print_students_by_teacher(StudentList *head, char *teacher) {
+  StudentList *current = head;
+  while (current != NULL) {
+    SubjectList *existing_subjects = current->student.student_subjects;
+    SubjectList *current_subjects = existing_subjects;
+    while (current_subjects != NULL) {
+      if (strcmp(current_subjects->subject.teacher, teacher) == 0) {
+        printf("%s \n" ,current->student.student_name);
+      }
+      current_subjects = current_subjects->next;
+    }
+    current = current->next;
+  }
+}
   
 int main () {
   SubjectList *subject_head = NULL;
@@ -413,6 +448,17 @@ int main () {
 
   printf("=================================================== \n");
   print_grade_per_subject(&student_with_id_1->student, 3);
+
+
+  printf("=================================================== \n");
+  print_students_by_teacher(student_head, "Mrs White");
+
+  print_students_by_teacher(student_head, "Mrs Brown");
+
+  print_students_by_teacher(student_head, "Mr Black");
+
+  printf("=================================================== \n");
+  print_teachers_by_student(student_head, 1);
 
   return 0;
 }
